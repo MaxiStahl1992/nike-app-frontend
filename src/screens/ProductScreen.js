f40dba636b7d79a4999d33e20899b577fcbc3436
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2023-05-26 14:48:47 +0200

    add navigation

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,14 +1,17 @@
-import { StyleSheet, View, Image, FlatList } from 'react-native';
+import { StyleSheet, View, Image, FlatList, Pressable } from 'react-native';
 import products from '../data/products';
 
-const ProductScreen = () => {
+const ProductScreen = ({ navigation }) => {
   return (
     <FlatList 
         data={products}
         renderItem={({ item }) => (
-          <View style={styles.itemContainer}>
+          <Pressable 
+            onPress={() => navigation.navigate('Product Details')}
+            style={styles.itemContainer}
+          >
             <Image source={{ uri: item.image }} style={styles.image} />
-          </View>
+          </Pressable>
         )}
         numColumns={2}
     />
